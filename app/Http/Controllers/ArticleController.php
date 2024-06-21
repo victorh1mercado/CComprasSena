453bb4e9beca7311423ce14880ab9f0e1cf09760
victorh1mercado <107508570+victorh1mercado@users.noreply.github.com>
2024-06-21 10:30:29 -0500

    foraneas
    se agrega crud con foraneas

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Category;
 
 class ArticleController extends Controller
 {
@@ -11,7 +12,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        return view('dashboard.article.index');
+        $article=Article::all();
+        return view('dashboard.article.index',['article'=>$article]);
     }
 
     /**
@@ -19,7 +21,8 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        $category=Category::all();
+        return view('dashboard.article.create',['category'=>$category]);
     }
 
     /**
@@ -27,7 +30,16 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $article= new Article();
+        $article->code=$request->input('code');
+        $article->name=$request->input('name');
+        $article->Sale_Price=$request->input('Sale_Price');
+        $article->stock=$request->input('stock');
+        $article->description=$request->input('description');
+        $article->state=$request->input('state');
+        $article->idcategory=$request->input('category');
+        $article->save();
+        return view("dashboard.article.message",['msg'=>"Articulo creado con exito"]);
     }
 
     /**
@@ -41,24 +53,36 @@ class ArticleController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $article=Article::find($id);
+        return view('dashboard.article.edit',['article'=>$article,'category'=>Category::all()]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $article= Article::find($id);
+        $article->code=$request->input('code');
+        $article->name=$request->input('name');
+        $article->Sale_Price=$request->input('Sale_Price');
+        $article->stock=$request->input('stock');
+        $article->description=$request->input('description');
+        $article->state=$request->input('state');
+        $article->idcategory=$request->input('category');
+        $article->save();
+        return view("dashboard.article.message",['msg'=>"Articulo actualizado con exito"]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $article=Article::find($id);
+        $article->delete();
+        return redirect("dashboard/article");
     }
 }
--- resources/views/dashboard/Article/index.blade.php
+++ resources/views/dashboard/Article/index.blade.php
@@ -1,12 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hola! esta sera la pagina de Articulos
-    aqui listaremos todos los articulos registrados en la base de datos.
-</body>
-</html>
+@extends('dashboard.master')
+@section('titulo','Articulos')
+@include('layouts/navigation')
+@section('contenido')
+
+<div class="container py-4">
+    <h1>Listado de Articulos</h1>
+    <br>
+    <a href="{{url('dashboard/article/create')}}" class="btn btn-primary btn-sm"> Nuevo articulo</a>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th>Id Producto</th>
+                <th>Codigo</th>
+                <th>Nombre</th>
+                <th>Precio de Venta</th>
+                <th>Stock</th>
+                <th>Descripcion</th>
+                <th>Estado</th>
+                <th>Categoria</th>
+                <th>FechaCreacion</th>
+                <th>FechaActualizacion</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($article as $article)
+            <tr>
+                <td scope="row">{{$article->id}}</td>
+                <td>{{$article->code}}</td>
+                <td>{{$article->name}}</td>
+                <td>{{$article->Sale_Price}}</td>
+                <td>{{$article->stock}}</td>
+                <td>{{$article->description}}</td>
+                <td>{{$article->state ? __('activo') : __('inactivo') }}</td>
+                <td>{{$article->category->name}}</td>
+                <td>{{$article->created_at}}</td>
+                <td>{{$article->updated_at}}</td>
+                <td>
+                    <a href="{{ url('dashboard/article/'.$article->id.'/edit') }}" class="bi bi-pencil"></a>
+                </td>
+                <td>
+                    <form action="{{ url('dashboard/article/'.$article->id) }}" method="post">
+                        @method("DELETE")
+                        @csrf
+                        <button class="bi bi-eraser-fill" type="submit" ></button>                                
+                    </form>
+                </td>
+
+            </tr>
+            <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
+</div>
+@endsection
